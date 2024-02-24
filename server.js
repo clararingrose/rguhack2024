25eb6c2b73ae7cfc6c2926bc1f31324d76e1626c
JakeMilne <91962700+JakeMilne@users.noreply.github.com>
2024-02-24 21:02:06 +0000

    broke :(

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var app = express();
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
-
+app.use(express.static('public'));
 
 // use res.render to load up an ejs view file
 // index page
