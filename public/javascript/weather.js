7e2badcfd32363600b9f5d8406e93cbf8ad02558
clararingrose <2001824@rgu.ac.uk>
2024-02-25 10:00:39 +0000

    update

---
+++ public/javascript/weather.js
@@ -0,0 +1,24 @@
+// Make a GET request to the API endpoint
+fetch('http://api.weatherapi.com/v1/forecast.json?key=b2bd0330a48f4135aca23234242502&q=57.13069126923598, -2.1037042191987134&days=7&aqi=no&alerts=no https://www.weatherapi.com/api-explorer.aspx#forecast')
+.then(response => {
+        // Check if the request was successful
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        // Parse the JSON response
+        return response.json();
+    })
+    .then(data => {
+        // Extract values
+        var output = "<table><tr><th>Date</th><th>Average Temp (°C)</th></tr>"
+        data.forecast.forecastday.forEach(j =>{
+            output+= "<tr><td>" + j.date + "</td><td>" + j.day.avgtemp_c + "</td></tr>"
+        })
+        output+="</table>"
+
+        // Update the content of the output div with the current temperature
+        document.getElementById('output').innerHTML = output;
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
