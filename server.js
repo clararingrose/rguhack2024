fe0a291f9dd78686e805e29633c9eeae89df4cb7
clararingrose <2001824@rgu.ac.uk>
2024-02-25 08:21:52 +0000

    calendar

--- server.js
+++ server.js
@@ -52,15 +52,4 @@ app.get('/treasure', function(req, res) {
 });
 
 // ar page
-app.get('/ar', function(req, res) {
-    res.render('pages/ar.ejs');
-});
-
-// winter gardens map page
-app.get('/wgmap', function(req, res) {
-    res.render('pages/wintergardensmap.ejs');
-});
-
-
-app.listen(8080);
-console.log('8080 is the magic port'); 
+app.get('/
